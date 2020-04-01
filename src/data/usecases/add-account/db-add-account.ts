752d21ebc4f345d3255863120c6f33914470cd35
anya <anya@pop-os.localdomain>
2020-04-01 11:38:31 -0300

    feat: ensure DbAddAccount calls Encrypter with correct password

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -3,7 +3,7 @@ import {
   AddAccountModel
 } from "../../../domain/usecases/addaccount";
 import { AccountModel } from "../../../domain/models/account";
-import { Encrypter } from "../protocols/encrypter";
+import { Encrypter } from "../../protocols/encrypter";
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter;
