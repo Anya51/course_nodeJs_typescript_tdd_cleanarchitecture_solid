c3cb0e75a71dddef6b31f38c27eec4ea879a62b1
anya <anya@pop-os.localdomain>
2020-04-30 11:14:02 -0300

    test: ensure AccountMongoRepository returns an account on sucess

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,26 @@
+import { MongooseHelper } from "../helpers/mongoose-helpers";
+import { AccountMongoRepository } from "./account";
+
+describe("Account Mongo Repository", () => {
+  beforeAll(async () => {
+    await MongooseHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongooseHelper.disconnect();
+  });
+
+  test("Should return an account on sucess ", async () => {
+    const sut = new AccountMongoRepository();
+    const account = await sut.add({
+      name: "any_name",
+      email: "any_email@mail.com",
+      password: "any_password",
+    });
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toBe("any_name");
+    expect(account.email).toBe("any_email@mail.com");
+    expect(account.password).toBe("any_password");
+  });
+});
